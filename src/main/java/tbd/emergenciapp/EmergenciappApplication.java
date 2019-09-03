e105137c7e4454ed0a771f28fee94261589eca6d
basil <basil.ossandon@usach.cl>
2019-09-03 16:43:36 -0400

    v0.0.1.2 Maven Dependency

---
+++ src/main/java/tbd/emergenciapp/EmergenciappApplication.java
@@ -0,0 +1,13 @@
+package tbd.emergenciapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergenciappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergenciappApplication.class, args);
+	}
+
+}
