c936992240405aa6345b931a11c72965d1d590a7
Maximiliano Orellana <maximiliano.orellana.a@usach.cl>
2019-09-09 17:53:50 -0300

    v0.0.4 Modified router

--- src/router.js
+++ src/router.js
@@ -1,22 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import NewDog from './views/NewDog.vue'
 import Emergency from './views/Emergency.vue'
 
 
 Vue.use(Router);
 var router = new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Emergency
-    },
-    {
-      path:'/new-dog',
-      name:'new-dog',
-      component: NewDog
-    },
     {
       path:'/emergencies',
       name:'emergency',
