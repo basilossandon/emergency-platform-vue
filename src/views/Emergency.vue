0f5e19c79e973ab952c7656db2d72f82426a1676
basil <basil.ossandon@usach.cl>
2019-09-15 01:13:06 -0300

    v0.3.1 Accordion added

--- src/views/Emergency.vue
+++ src/views/Emergency.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <h1>Todos las emergencias</h1>
-    <ul class="item-list">
-      <li v-for="emergency in emergencies" :key="emergency.id">
-        <img :src="'https://loremflickr.com/160/120/earthquake?lock='+emergency.id"/>
+
+
+
+      <div role="tablist" v-for="emergency in emergencies" :key="emergency.id" v-bind:title="{ 'emergencyName': emergency.active }">
+      <el-collapse v-model="activeName" accordion>
+      <el-collapse-item v-bind:title="emergency.name" name="emergency.id">
+                <img :src="'https://loremflickr.com/160/120/earthquake?lock='+emergency.id"/>
         {{emergency.name}}
-      </li>
-    </ul>
+        <div>Consistent with real life: in line with the process and logic of real life, and comply with languages and habits that the users are used to;</div>
+        <div>Consistent within interface: all elements should be consistent, such as: design style, icons and texts, position of elements, etc.</div>
+    </el-collapse-item>
+    </el-collapse>
+      </div>
     <div v-if="emergencies.length==0" class="empty-list">
       <em>No se han cargado los datos</em>
     </div>
