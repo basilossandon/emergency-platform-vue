c47d1e8a582cc75d171711422fcbacfd95c66547
basil <basil.ossandon@usach.cl>
2019-09-15 02:04:31 -0300

    v0.3.2 CSS Updated

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="app">
         <NavBar class="app__navbar"></NavBar>
-        <router-view></router-view>
+        <div class="main-container">
+            <div class="content-wrapper">
+            <router-view></router-view>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
+import 'element-ui/lib/theme-chalk/reset.css'
+
 export default {
     name: 'app',
     components: {
@@ -26,5 +32,19 @@ export default {
 
 body {
     margin: 0px;
+}
+
+.main-container {
+  text-align: center;
+  margin-top: 50px;
+  margin-left: 50px;
+  margin-right: 50px;
+  min-height: 50vh;
+  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    -webkit-border-radius: 30px;
+  border-radius: 15px;
+}
+.content-wrapper{
+    padding:15px;
 }
 </style>
