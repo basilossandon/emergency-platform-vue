be5194f81fd030c219edf3d316fa141eb9877220
Basil Ossandon <basil.ossandon@usach.cl>
2019-10-07 19:14:28 -0300

    1.2.3 Import works

--- src/views/Import.vue
+++ src/views/Import.vue
@@ -1,89 +1,64 @@
 <template>
-  <div class="Import">
-    <h1>Import</h1>
-    <vue-csv-import
-      v-model="csv"
-      url="localhost:4567/volunteers/"
-      :map-fields="['Column 1', 'Column 2', 'Column 3']"
-    >
-      <template slot="error">
-        File type is invalid
-      </template>
-
-      <template slot="thead">
-        <tr>
-          <th>My Fields</th>
-          <th>Column</th>
-        </tr>
-      </template>
-
-      <template slot="next" slot-scope="{load}">
-        <div class="button-emergency-wrapper">
-          <el-button
-            type="primary"
-            round
-            icon="el-icon-upload"
-            @click.prevent="load"
-            >Load File</el-button
-          >
-        </div>
-      </template>
-
-      <template slot="submit" slot-scope="{submit}">
-        <div class="button-emergency-wrapper">
-          <el-button
-            type="primary"
-            round
-            icon="el-icon-upload"
-            @click.prevent="submit"
-            >Post</el-button
-          >
-        </div>
-      </template>
-    </vue-csv-import>
-
-    <div class="mt-2">
-      {{ csv }}
+  <div class="container">
+    <div class="large-12 medium-12 small-12 cell">
+      <label>
+        File
+        <input type="file" id="file" ref="file" v-on:change="handleFileUpload()" />
+      </label>
+      <button v-on:click="submitFile()">Submit</button>
     </div>
-    <p>
-      Note: You can only import ".csv" files with volunteers data in this
-      format: "column1: name", "column2: age", "column3: sex"
-    </p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "app",
-    data() {
-      return {
-        csv: null
-      };
+export default {
+  /*
+      Defines the data used by the component
+    */
+  data() {
+    return {
+      file: ""
+    };
+  },
+
+  methods: {
+    /*
+        Submits the file to the server
+      */
+    submitFile() {
+      /*
+                Initialize the form data
+            */
+      let formData = new FormData();
+
+      /*
+                Add the form data we need to submit
+            */
+      formData.append("file", this.file);
+
+      /*
+          Make the request to the POST /single-file URL
+        */
+      this.$http
+        .post("/volunteers/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
+        })
+        .then(function() {
+          console.log("SUCCESS!!");
+        })
+        .catch(function() {
+          console.log("FAILURE!!");
+        });
+    },
+
+    /*
+        Handles a change on the file upload
+      */
+    handleFileUpload() {
+      this.file = this.$refs.file.files[0];
     }
-  };
+  }
+};
 </script>
-
-<style>
-  #app {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-
-  .container {
-    text-align: left;
-  }
-
-  pre code {
-    background-color: #eee;
-    border: 1px solid #999;
-    display: block;
-    padding: 20px;
-  }
-
-  #app .form {
-    text-align: left;
-  }
-</style>
